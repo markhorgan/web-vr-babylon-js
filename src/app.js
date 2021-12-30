5e4257cf84b099439de5e3af8bc25d6045567a56
Mark Horgan <public@markhorgan.com>
2021-12-30 17:34:04 +0000

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,122 @@
+import { 
+  Engine, 
+  FreeCamera, 
+  Vector3, 
+  Scene, 
+  MeshBuilder, 
+  StandardMaterial, 
+  Color3,
+  HemisphericLight,
+  Ray } from 'babylonjs';
+
+const objectUnselectedColor = Color3.FromHexString('#5853e6');
+const objectSelectedColor = Color3.FromHexString('#f0520a');
+
+class App {
+  constructor() {
+    // Setup Babylon.js
+    const canvas = document.getElementById('canvas');
+    const engine = new Engine(canvas, true);
+    this.scene = new Scene(engine);
+    const camera = new FreeCamera('camera', new Vector3(0, 3, -3), this.scene);
+    camera.setTarget(Vector3.Zero());
+    camera.attachControl(canvas, true);
+    
+    this.createScene();
+    this.initXR();
+
+    engine.runRenderLoop(() => {
+      if (this.controllers) {
+        this.controllers.forEach(controller => {
+          this.handleController(controller);
+        });
+      }
+
+      if (this.scene) {
+        this.scene.render();
+      }
+    });
+
+    window.addEventListener('resize', () => {
+      engine.resize();
+    });
+  }
+
+  createScene() {
+    // Box
+    const box = MeshBuilder.CreateBox('box', { size: 0.5 }, this.scene);
+    const boxMaterial = new StandardMaterial('boxMaterial', this.scene);
+    boxMaterial.diffuseColor = objectUnselectedColor;
+    box.material=boxMaterial;
+
+    // Light
+    const light = new HemisphericLight('light', new Vector3(-0.5, -1, -0.25), this.scene);
+    light.diffuse = Color3.FromHexString('#ffffff');
+    light.groundColor = Color3.FromHexString('#bbbbff');
+    light.intensity = 1;
+  }
+
+  async initXR() {
+    // this.xr: WebXRDefaultExperience https://doc.babylonjs.com/typedoc/classes/babylon.webxrdefaultexperience
+    const sessionMode = 'immersive-vr'
+    this.xr = await this.scene.createDefaultXRExperienceAsync({ uiOptions: { sessionMode }});
+    const isSupported = await this.xr.baseExperience.sessionManager.isSessionSupportedAsync(sessionMode);
+    if (!isSupported) {
+      alert('WebXR not supported')
+    } else {
+      this.controllers = [];
+      // this.xr.input: WebXRInput https://doc.babylonjs.com/typedoc/classes/babylon.webxrinput
+      this.xr.input.onControllerAddedObservable.add(controller => {
+        // controller: WebXRInputSource https://doc.babylonjs.com/typedoc/classes/babylon.webxrinputsource
+        controller.userData = {trigger: { pressed: false, pressedPrev: false }};
+        this.controllers.push(controller);
+        controller.onMotionControllerInitObservable.add(motionController => {
+          // motionController: WebXRAbstractMotionController https://doc.babylonjs.com/typedoc/classes/babylon.webxrabstractmotioncontroller
+          const ids = motionController.getComponentIds();
+          console.log(ids);
+          const triggerComponent = motionController.getComponent('xr-standard-trigger');
+          triggerComponent.onButtonStateChangedObservable.add(() => {
+            controller.userData.trigger.pressed = triggerComponent.pressed;
+          });
+        });
+      });
+    }
+  }
+
+  handleController(controller) {
+    if (controller.userData.trigger.pressed) {
+      if (!controller.userData.trigger.pressedPrev) {
+        // Trigger pressed
+        const ray = this.getControllerRay(controller);
+        const hit = this.scene.pickWithRay(ray);
+        if (hit && hit.hit) {
+          this.selectedMesh = hit.pickedMesh;
+          this.selectedMesh.material.diffuseColor = objectSelectedColor;
+          this.selectedMeshDistance = Vector3.Distance(this.selectedMesh.position, controller.pointer.position);
+        }
+      } else if (this.selectedMesh) {
+        // Move selected object so it's always the same distance from controller
+        const ray = this.getControllerRay(controller);
+        const moveVector = ray.direction.scale(this.selectedMeshDistance);
+        this.selectedMesh.position.copyFrom(controller.pointer.position.add(moveVector));
+      }
+    } else if (controller.userData.trigger.pressedPrev) {
+      // Trigger released
+      if (this.selectedMesh != null) {
+        this.selectedMesh.material.diffuseColor = objectUnselectedColor;
+        this.selectedMesh = null;
+      }
+    }
+    controller.userData.trigger.pressedPrev = controller.userData.trigger.pressed;
+  }
+
+  getControllerRay(controller) {
+    const ray = new Ray(new Vector3(), new Vector3());
+    controller.getWorldPointerRayToRef(ray);
+    return ray;
+  }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  new App();
+});
